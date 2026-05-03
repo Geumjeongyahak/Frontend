328c8f10a08038011ca7d9f18510215e7bfe295d
byunggil723 <qudrlf72@naver.com>
2026-05-03 22:10:40 +0900

    hotfix: 배포 환경 API base URL 설정

--- api/client/publicClient.ts
+++ api/client/publicClient.ts
@@ -1,9 +1,6 @@
 import axios from "axios";
 
-const baseURL =
-  process.env.NEXT_PUBLIC_API_BASE_URL ??
-  process.env.NEXT_PUBLIC_API_URL ??
-  "http://localhost:8080";
+const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL ?? "http://localhost:8080";
 
 export const baseClientConfig = {
   baseURL,
